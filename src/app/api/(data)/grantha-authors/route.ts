39bc66d9a43e5ccd1225421b365b11dabeedacb4
harshith <harshith.raju0710@gmail.com>
2025-04-05 17:10:05 +0530

    adding authors page created and fixed scrollbar

--- src/app/api/(data)/grantha-authors/route.ts
+++ src/app/api/(data)/grantha-authors/route.ts
@@ -4,27 +4,27 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(request: NextRequest) {
     try {
         const reqBody = await request.json();
-        const { authorName, birthYear, deathYear, bio, scribeName } = reqBody;
+        const { author_name, birth_year, death_year, bio, scribe_name } = reqBody;
 
-        if(!authorName) {
+        if(!author_name) {
             return NextResponse.json("Author name is required", { status: 400 });
         }
 
-        if(!scribeName) {
+        if(!scribe_name) {
             return NextResponse.json("Scribe Name is required", { status: 400 });
         }
 
         await db.author.create({
             data: {
-                author_name: authorName,
-                birth_year: birthYear,
-                death_year: deathYear,
+                author_name: author_name,
+                birth_year: birth_year,
+                death_year: death_year,
                 bio: bio,
-                scribe_name: scribeName
+                scribe_name: scribe_name,
             },
         })
 
-        return NextResponse.json(`Grantha Author ${authorName} created successfully.`, { status: 200 });
+        return NextResponse.json(`Grantha Author ${author_name} created successfully.`, { status: 200 });
 
 
     } catch (error: any) {
